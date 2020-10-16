3eb87891975e9ca6f67fe9ccabd09ebe26fca3ca
d <ledaiduongvnth@gmail.com>
2020-10-16 10:27:01 +0700

    improve mqtt client

--- client/mqtt_client.py
+++ client/mqtt_client.py
@@ -1,5 +1,9 @@
 import paho.mqtt.client as mqtt
-
+import time
+import json
+from random import seed
+from random import randint
+seed(1)
 
 def on_connect(mqttc, obj, flags, rc):
     print("rc: " + str(rc))
@@ -10,8 +14,7 @@ def on_message(mqttc, obj, msg):
 
 
 def on_publish(mqttc, obj, mid):
-    print("mid: " + str(mid))
-    pass
+    print('on_publish')
 
 
 def on_subscribe(mqttc, obj, mid, granted_qos):
@@ -22,22 +25,37 @@ def on_log(mqttc, obj, level, string):
     print(string)
 
 
-# If you want to use a specific client id, use
-# mqttc = mqtt.Client("client-id")
-# but note that the client id must be unique on the broker. Leaving the client
-# id parameter empty will generate a random id for you.
-mqttc = mqtt.Client()
-mqttc.on_message = on_message
-mqttc.on_connect = on_connect
-mqttc.on_publish = on_publish
-mqttc.on_subscribe = on_subscribe
-# Uncomment to enable debug messages
-# mqttc.on_log = on_log
-mqttc.connect("localhost", 1883, 60)
+def send_data_from_bound_device():
+    mqttc = mqtt.Client()
+    mqttc.on_message = on_message
+    mqttc.on_connect = on_connect
+    mqttc.on_publish = on_publish
+    mqttc.on_subscribe = on_subscribe
+    mqttc.connect("localhost", 1883, 60)
+    mqttc.loop_start()
 
-mqttc.loop_start()
+    dataindata = {
+        "may01": {"status": "RUN", "period": randint(0, 1000), "product": randint(0, 1000),
+                  "power": randint(0, 1000)},
+        "may02": {"status": "OFF", "period": randint(0, 1000), "product": randint(0, 1000),
+                  "power": randint(0, 1000)},
+        "may03": {"status": "ON", "period": randint(0, 1000), "product": randint(0, 1000),
+                  "power": randint(0, 1000)},
+        "may04": {"status": "ON", "period": randint(0, 1000), "product": randint(0, 1000),
+                  "power": randint(0, 1000)}
+    }
 
-print("class")
-infot = mqttc.publish("gggggggggg", "{sfdfdf:sfsdfdfd}", qos=0)
+    data = {
+        'messagetype': 3,
+        "factory_id": "rostek",
+        "timestamp": int(time.time()),
+        "data": dataindata
+    }
+    payload = json.dumps(data)
+    while 1:
+        mqttc.publish("topic", payload, qos=0)
+        print('Publishing message: \'{}\' to {}'.format(payload, "topic"))
+        time.sleep(1)
 
-infot.wait_for_publish()
+
+send_data_from_bound_device()
